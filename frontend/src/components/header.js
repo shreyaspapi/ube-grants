6f8b32c299beb333854ca3ed4b98132eb338649a
harshitsinghai77 <harshitsinghai77@gmail.com>
2022-12-09 19:49:58 +0530

    add connectWallet

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,4 +1,10 @@
+import { Web3Button } from "@web3modal/react";
+
 const Header = () => {
+  const ConnectWalletButton = () => {
+    return <Web3Button />;
+  };
+
   return (
     <header>
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
@@ -23,6 +29,8 @@ const Header = () => {
             <div className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
               Get started
             </div>
+
+            <ConnectWalletButton />
 
             <button
               data-collapse-toggle="mobile-menu-2"
